c8d2ee64d206ffcdf647d00deaa5aacdd68f9111
Aryan Singh <ary1733@gmail.com>
2021-08-22 19:19:38 +0530

    part1 done

--- Assn1/FullAdder.v
+++ Assn1/FullAdder.v
@@ -7,9 +7,18 @@ module FA_Struct(a, b, cin, cout, sum);
 
  xor U1  (w1, a, b);
  xor U2  (sum, w1, cin);
- and U3  (w2, a, b);
- and U4  (w3, a, cin);
- and U5  (w4, b, cin);
- or  U6  (cout, w2, w3, w4);
+ and U3  (w2, cin, w1);
+ and U4  (w3, a, b);
+ or  U6  (cout, w2, w3);
 endmodule
 
+// Truth Table
+//      a   b   cin sum cout
+//      0   0   0   0   0
+//      0   0   1   1   0
+//      0   1   0   1   0
+//      0   1   1   0   1
+//      1   0   0   1   0
+//      1   0   1   0   1
+//      1   1   0   0   1
+//      1   1   1   1   1
--- Assn1/halfAdder.v
+++ Assn1/halfAdder.v
@@ -1,7 +1,14 @@
 
-module halfAdder (a, b, cout, sum);
+module halfAdder (a, b, sum, cout);
 input a,b;              // inputs
 output sum,cout;        // outputs
-    and U1(sum,a,b);    
-    xor U2(cout,a,b);
+    and U1(cout,a,b);    
+    xor U2(sum,a,b);
 endmodule
+
+// Truth Table
+//      a   b   sum cout
+//      0   0   0   0
+//      0   1   1   0
+//      1   0   1   0
+//      1   1   1   1
