7473b1f3744aca779f3ad574c4375b368bec00e2
Aryan Singh <ary1733@gmail.com>
2021-08-26 13:29:51 +0530

    Synthesis Report

--- Assn1/part1/RippleCarryAdder.v
+++ Assn1/part1/RippleCarryAdder.v
@@ -5,6 +5,21 @@
 `ifndef _RCA_v_
 `define _RCA_v_
 
+
+module rca4(A, B, cin1, S, cout1);
+    input[3:0] A, B;
+    input cin1;
+    output[3:0] S;
+    output cout1;
+
+    wire c1, c2, c3;
+    // 4 instantiated 1-bit full adders
+    FA_Struct fa0 (A[0], B[0], cin1, c1, S[0]);
+    FA_Struct fa1 (A[1], B[1], c1, c2, S[1]);
+    FA_Struct fa2 (A[2], B[2], c2, c3, S[2]);
+    FA_Struct fa3 (A[3], B[3], c3, cout1, S[3]);
+endmodule
+
 module rca8(A, B, cin1, S, cout1);
     input[7:0] A, B;
     input cin1;
