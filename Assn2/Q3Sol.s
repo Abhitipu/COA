641211df883c256e2607a6768c20d404f3ffbcdb
Aryan Singh <ary1733@gmail.com>
2021-09-02 16:48:19 +0530

    Sqrt n Prime Testing

--- Assn2/Q3Sol.s
+++ Assn2/Q3Sol.s
@@ -22,6 +22,7 @@ message3:
 # n:                $s0
 # i:                $s1
 # numberOfFactors:  $s2
+# i^2:              $s3
 main:
     li      $v0, 4                          # v0 <- 4(print string)
     la      $a0, question                   # loads the address of question on a0
@@ -35,14 +36,18 @@ main:
     bne     $t0, $zero, SCheck              # if t0 != zero then
     move    $s2, $zero                      # s2 initialized as zero
     li      $s1, 2                          # s1 <- 1
-
+    li      $s3, 4                          # s3 will store the value of i^2
 beginloop:
-    beq     $s0, $s1, EndLoop               # loop termination Condition
+    slt     $t0, $s0, $s3                   # set t0 = 1, if n < i^2
+    bne     $t0, $zero, EndLoop             # loop termination Condition
     div     $s0, $s1                        # Hi = $s0 % $s1
     mfhi    $t0                             # t0 = remainder
     bne     $t0, $zero, elsePart            # if remainder != 0 goto elsePart
     addi    $s2, $s2, 1                     # number of factors++
 elsePart:
+    add     $t0, $s1, $s1                   # to add 2*(i) + 1 to make i^2 -> (i+1)^2
+    addi    $t0, $t0, 1                     # t0 = i + i + 1
+    add     $s3, $s3, $t0                   # s3 = (i+1)^2
     addi    $s1, $s1, 1                     # i++
     b       beginloop                       # loop
 EndLoop:
