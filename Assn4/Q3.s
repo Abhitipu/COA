8f8f7d78740af5f551e3719e9c5d1869bbf5eba6
Aryan Singh <ary1733@gmail.com>
2021-09-21 00:32:24 +0530

    recurse search subroutine

--- Assn4/Q3.s
+++ Assn4/Q3.s
@@ -29,6 +29,7 @@ recurse_search_continue:
     addi            $sp, $sp, -24           # Allocating some space for Arguments, return address and frame pointor
     sw              $fp, ($sp)              # M[sp] = fp
     move            $fp, $sp                # fp = sp
+    addi            $sp, $sp, 8             # 2 int space allocated for mid1 and mid2
     sw              $a0, 20($fp)            # fp[20] = a0
     sw              $a1, 16($fp)            # fp[16] = a1
     sw              $a2, 12($fp)            # fp[12] = a2
@@ -44,5 +45,54 @@ recurse_search_continue:
     sub             $t2, $a2, $t0           # mid2= end - (end - start)/3
     sw              $t2, -8($fp)            # fp[-8] = mid2
 
+    # Base Cases
+    sll             $t0, $t1, 2             # t0 = mid1*4
+    add             $t0, $t0, $a0           # t0 = A + mid1 * 4
+    lw              $t0, ($t0)              # t0 = A[mid1]
+    bne             $t0, $a3, recurse_search_continue1  # if key != A[mid1] search continues
+    move            $v0, $t1                # v0 = mid1
+    j               recurse_search_return   # jump to recurse_search_return and restore fp & sp and return
+
+recurse_search_continue1:
+    sll             $t0, $t2, 2             # t0 = mid2*4
+    add             $t0, $t0, $a0           # t0 = A + mid2 * 4
+    lw              $t0, ($t0)              # t0 = A[mid2]
+    bne             $t0, $a3, recurse_search_continue2  # if key != A[mid2] search continues
+    move            $v0, $t2                # v0 = mid2
+    j               recurse_search_return   # jump to recurse_search_return and restore fp & sp and return
+
+    # recursive cases    
+recurse_search_continue2:
+    sll             $t0, $t1, 2             # t0 = mid1*4
+    add             $t0, $t0, $a0           # t0 = A + mid1 * 4
+    lw              $t0, ($t0)              # t0 = A[mid1]
+    slt             $t0, $a3, $t0           # t0 = (key < A[mid1]) ? 1 : 0
+    bne             $t0, $zero, recurse_search_continue3    # if key>=A[mid1] search continues
+    addi            $a2, $t1, -1            # a2 = mid1 - 1
+    jal             recurse_search          # recursive call, v0 stores the ans
+    j               recurse_search_return   # jump to recurse_search_return and restore fp & sp & ra and return
+
+recurse_search_continue3:
+    sll             $t0, $t2, 2             # t0 = mid2*4
+    add             $t0, $t0, $a0           # t0 = A + mid2 * 4
+    lw              $t0, ($t0)              # t0 = A[mid2]
+    slt             $t0, $t0, $a3           # t0 = (A[mid2] < key) ? 1 : 0
+    bne             $t0, $zero, recurse_search_continue4    # if key<=A[mid2] search continues
+    addi            $a1, $t2, 1             # a1 = mid2 + 1
+    jal             recurse_search          # recursive call, v0 stores the ans
+    j               recurse_search_return   # jump to recurse_search_return and restore fp & sp & ra and return
+               
+recurse_search_continue4:
+    addi            $a1, $t1, 1             # a1 = mid1 + 1
+    addi            $a2, $t2, -1            # a2 = mid2 - 1
+    jal             recurse_search          # recursive call, v0 stores the ans
+    j               recurse_search_return   # jump to recurse_search_return and restore fp & sp & ra and return
+
+recurse_search_return:
+    lw              $ra, 4($fp)             # ra restored
+    move            $sp, $fp                
+    lw              $fp, ($fp)              # fp restored
+    addi            $sp, $sp, 24            # sp restored
+    jr              $ra                     # jump to ra
 
     
