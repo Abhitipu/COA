266ade12c6221e317c6d1ebdc64e98d44345dd6a
Aryan Singh <ary1733@gmail.com>
2021-09-27 15:02:45 +0530

    Left Shift Parallel Load Added/ But not Async Load

--- Assn5/Q4/ShiftRegister.v
+++ Assn5/Q4/ShiftRegister.v
@@ -20,43 +20,24 @@ Abhinandan De (19CS10069)
 // module multiplexer_struct(x , u , v , z);
 // z = xu + x_bar.v
 
-module ShiftLoadReg4_struct(
-        input[3:0] load,
+module ShiftLoadReg32_struct(
+        input[31:0] load,
         input shiftin,
         input sel, 
         input reset, 
         input clk,
-        output[3:0] state
+        output[31:0] state
     );
 	
-    wire[4:0] w;
-    wire [3:0] wint;
+    wire[0:32] w;
+    wire [0:31] wint;
 	// sel = 1 => set load
-    // d_flipflop ff[7:0] (A, Q, reset clk);
-    multiplexer_struct[3:0] mux(sel, load, w[0:3], wint[0:3]);
-    dff_struct[3:0] dff(wint[0:3], clk, reset, w[1:4]);
-    assign state[3:0] = w[1:4]
+    multiplexer_struct mux[31:0] (sel, load, w[1:32], wint);
+    dff_struct dff[31:0] (wint, clk, reset, w[0:31]);
 
+    assign state[31:0] = {w[0:31]};
 
-    /*
-    multiplexer_struct mux3(sel, load[3], w[0], wint[0]);
-    dff_struct dff3(wint[0], clk, reset, w[1]);
-    assign state[3] = w[1];
-
-    multiplexer_struct mux2(sel, load[2], w[1], wint[1]);
-    dff_struct dff2(wint[1], clk, reset, w[2]);
-    assign state[2] = w[2];
-
-    multiplexer_struct mux1(sel, load[1], w[2], wint[2]);
-    dff_struct dff1(wint[2], clk, reset, w[3]);
-    assign state[1] = w[3];
-
-    multiplexer_struct mux0(sel, load[0], w[3], wint[3]);
-    dff_struct dff0(wint[3], clk, reset, w[4]);
-    assign state[0] = w[4];
-    */
-
-    assign w[0] = shiftin;
+    assign w[32] = shiftin;
 
 endmodule
 
