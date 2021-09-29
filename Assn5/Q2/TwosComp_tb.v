bfdbe0bcd3abf4d9af7950ec1bf201f5c0b9ef18
Aryan Singh <ary1733@gmail.com>
2021-09-29 18:12:30 +0530

    I think this is final

--- Assn5/Q2/TwosComp_tb.v
+++ Assn5/Q2/TwosComp_tb.v
@@ -23,9 +23,9 @@ module TwosComp_tb;
 
     initial begin
 
-        reset = 1; bit = 0;#10;
-        reset = 0; bit = 0; #10;
-        bit = 0; #10;
+        reset = 1; bit = 0;#10; // reset for starting 10ns
+        reset = 0; bit = 0; #10;// remove reset and wait 10ns
+        bit = 0; #10;           // start feeding input 
         bit = 0; #10;
         bit = 0; #10;
         bit = 1; #10;
@@ -38,9 +38,9 @@ module TwosComp_tb;
         bit = 1; #10;
         bit = 1; #20;
 		  
-        reset = 1; bit = 0;#10;
+        reset = 1; bit = 0;#10; // test 2
         reset = 0; bit = 0; #10;
-        bit = 0; #10;
+        bit = 0; #10;           // test 2 - starts input bit
         bit = 0; #10;
         bit = 0; #10;
         bit = 1; #10;
@@ -55,7 +55,7 @@ module TwosComp_tb;
 
         reset = 1; bit = 0;#10;
         reset = 0; bit = 0; #10;
-        bit = 1; #10;
+        bit = 1; #10;           // test 3 - starts input bit
         bit = 0; #10;
         bit = 0; #10;
         bit = 1; #10;
