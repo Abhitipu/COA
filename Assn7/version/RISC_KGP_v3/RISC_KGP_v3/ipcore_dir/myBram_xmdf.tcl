cbf4e6517cb99690cd17ce35229c3a6808bc993b
Abhitipu <abhinandan0316@gmail.com>
2021-11-10 10:52:44 +0530

    Added separate mem init file | infinite loop at the end

--- Assn7/version/RISC_KGP_v3/RISC_KGP_v3/ipcore_dir/myBram_xmdf.tcl
+++ Assn7/version/RISC_KGP_v3/RISC_KGP_v3/ipcore_dir/myBram_xmdf.tcl
@@ -223,10 +223,6 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path myBram.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path myBram.sym
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
-incr fcount
-
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path myBram.v
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
 incr fcount
